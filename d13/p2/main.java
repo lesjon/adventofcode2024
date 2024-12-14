634207ef875efca33357e972a10b4dc3152d8f07
Leon Klute <leonklute@live.nl>
2024-12-14 11:43:34 +0100

    d13p2

--- d13/p2/main.java
+++ d13/p2/main.java
@@ -19,7 +19,7 @@ record Vector(long x, long y){
 }
 
 record Machine(Vector a, Vector b, Vector prize){
-    enum Button{ A,B}
+    enum Button{ A, B }
 
     Button cheapestButton(){
         if(a.magnitude()/3 > b.magnitude()){
@@ -31,27 +31,6 @@ record Machine(Vector a, Vector b, Vector prize){
 }
 
 enum State{A,B,PRIZE, SEPARATOR};
-
-record Position(Vector location, long aPresses, long bPresses){
-
-    long cost() {
-        return (3L * aPresses) + bPresses;
-    }
-}
-
-void aStar(Position curr, Machine machine, PriorityQueue<Position> positions){
-    // println("aStar(" + curr + ", " + positions.size());
-    var a = new Position(curr.location().add(machine.a()), curr.aPresses() + 1, curr.bPresses());
-    if(a.location().x() <= machine.prize().x() && a.location().y() <= machine.prize().y()){
-        if(!positions.contains(a))
-            positions.add(a);
-    }
-    var b = new Position(curr.location().add(machine.b()), curr.aPresses(), curr.bPresses() + 1);
-    if(b.location().x() <= machine.prize().x() && b.location().y() <= machine.prize().y()){
-        if(!positions.contains(b))
-            positions.add(b);
-    }
-}
 
 void main() throws Exception {
     var lines = Files.lines(Path.of("input.txt")).toList();
@@ -72,8 +51,8 @@ void main() throws Exception {
                 b = new Vector(x, y);
                 yield State.PRIZE;}
             case PRIZE -> {
-                long x = /* 10000000000000L + */ parseLong(line.substring(line.indexOf('X')+2,line.indexOf(',')));
-                long y = /* 10000000000000L + */ parseLong(line.substring(line.indexOf('Y')+2,line.length()));
+                long x = 10000000000000L + parseLong(line.substring(line.indexOf('X')+2,line.indexOf(',')));
+                long y = 10000000000000L + parseLong(line.substring(line.indexOf('Y')+2,line.length()));
                 machines.add(new Machine(a,b,new Vector(x, y)));
                 yield State.SEPARATOR;}
             case SEPARATOR -> State.A;
@@ -82,43 +61,25 @@ void main() throws Exception {
     println(machines);
     long total = 0;
     for (Machine machine : machines) {
-        var startButton = machine.cheapestButton();
-        switch(startButton){
-            case A -> {
-                println("A");
-                long max = Math.min(machine.prize().x()/machine.a().x(), machine.prize().y()/machine.a().y());
-                for (long i = max; i >= 0 ; i--) {
-                    var pos = new Position(machine.prize().subtract(machine.a().mult(i)), i, 0);
-                    if (pos.location().x() % machine.b().x() == 0 && pos.location().y() % machine.b().y() == 0){
-                        var xBPresses = pos.location().x()/machine.b().x();
-                        var yBPresses = pos.location().y()/machine.b().y();
-                        if(xBPresses == yBPresses){
-                            var finalPos = new Position(pos.location().subtract(machine.b().mult(xBPresses)), pos.aPresses(), xBPresses);
-                            total += finalPos.cost();
-                            break;
-                        }
-                    }
-                }
-            }
-            case B -> {
-                println("B");
-                long max = Math.min(machine.prize().x()/machine.b().x(), machine.prize().y()/machine.b().y());
-                println("max" + max);
-                for (long i = max; i >= 0 ; i--) {
-                    var pos = new Position(machine.prize().subtract(machine.b().mult(i)), i, 0);
-                    if (pos.location().x() % machine.a().x() == 0 && pos.location().y() % machine.a().y() == 0){
-                        var xAPresses = pos.location().x()/machine.a().x();
-                        var yAPresses = pos.location().y()/machine.a().y();
-                        if(xAPresses == yAPresses){
-                            var finalPos = new Position(pos.location().subtract(machine.a().mult(xAPresses)), xAPresses, pos.bPresses());
-                            total += finalPos.cost();
-                            break;
-                        }
-                    }
-                }
-            }
+        // machine.a().x() * a + machine.b().x() * b == machine.prize().x();
+        // machine.a().y() * a + machine.b().y() * b == machine.prize().y();
+        //
+        // a == machine.prize().x() / machine.a().x() - machine.b().x() * b / machine.a().x() ;
+        // machine.a().y() * a + machine.b().y() * b == machine.prize().y();
+        //
+        // machine.a().y() * machine.prize().x() / machine.a().x() - machine.a().y() * machine.b().x() * b / machine.a().x() + machine.b().y() * b == machine.prize().y();
+        //
+        // (machine.a().y() * machine.b().x() / machine.a().x()- machine.b().y()) * b == -machine.prize().y() + machine.a().y() * machine.prize().x() / machine.a().x();
+        
+        double bPresses = (-(double)machine.prize().y() + (double)machine.a().y() * (double)machine.prize().x() / (double)machine.a().x() ) / ((double)machine.a().y() * (double)machine.b().x() / (double)machine.a().x()- (double)machine.b().y());
+        double aPresses = ((double)machine.prize().x() / (double)machine.a().x() - (double)machine.b().x() * bPresses / (double)machine.a().x());
+        if(
+            machine.a().x() * Math.round(aPresses) + machine.b().x() * Math.round(bPresses) == machine.prize().x()
+            &&
+            machine.a().y() * Math.round(aPresses) + machine.b().y() * Math.round(bPresses) == machine.prize().y()
+        ){
+            total += aPresses*3L+bPresses; 
         }
-        break;
     }
     println(total);
 }
