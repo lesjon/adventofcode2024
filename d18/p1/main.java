2eb3c9c515cc1632398fb655fbd3d263c53ac967
Leon Klute <leonklute@live.nl>
2024-12-18 10:56:29 +0100

    snel

--- d18/p1/main.java
+++ d18/p1/main.java
@@ -24,7 +24,7 @@ record Vector(int x, int y){
 
 static final Vector TARGET = new Vector(MAX, MAX);
 
-record State(Vector pos, int steps){}
+record State(Vector pos, int steps, Set<Vector> path){}
 
 String display(Collection<Vector> bytes, Vector currPos){
     var sb = new StringBuilder();
@@ -51,11 +51,21 @@ int distanceFromTarget(Vector pos){
 void main() throws Exception {
     List<Vector> bytes = Files.lines(Path.of(FILE)).map(Vector::from).toList();
     println(bytes);
+    Set<Vector> wanted = new HashSet<>();
+    for (int y = 0; y <= MAX; y++) {
+        for (int x = 0; x <= MAX; x++) {
+            wanted.add(new Vector(x,y));
+        }
+    }
+
 
     for (int i = BYTES_FALLEN; i < bytes.size(); i++) {
+        if(!wanted.contains(bytes.get(i-1))) {
+            continue;
+        }
         Set<Vector> visited = new HashSet<>();
         PriorityQueue<State> queue = new PriorityQueue<>((lhs, rhs) -> Integer.compare(lhs.steps, rhs.steps));
-        queue.add(new State(new Vector(0,0), 0));
+        queue.add(new State(new Vector(0,0), 0, Set.of()));
         boolean reachedTarget = false;
         while(queue.size() > 0) {
             // println(queue);
@@ -65,6 +75,7 @@ void main() throws Exception {
             if(curr.pos.equals(TARGET)){
                 println(curr.steps);
                 reachedTarget = true;
+                wanted = curr.path;
                 break;
             }
             for (var next : List.of(curr.pos.add(0,-1), curr.pos.add(1,0), curr.pos.add(0,1), curr.pos.add(-1,0))) {
@@ -74,7 +85,9 @@ void main() throws Exception {
                 if(hitsByte) continue;
                 if(visited.contains(next)) continue;
                 visited.add(next);
-                queue.add(new State(next, curr.steps+1));
+                var newPath = new HashSet<>(curr.path);
+                newPath.add(next);
+                queue.add(new State(next, curr.steps+1, newPath));
             }
         }
         if(!reachedTarget){
