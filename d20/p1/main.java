c96e15e33199473ae303789dcac5df4763707632
Leon Klute <leonklute@live.nl>
2024-12-20 19:37:44 +0100

    d20p1

--- d20/p1/main.java
+++ d20/p1/main.java
@@ -16,7 +16,7 @@ int width = 0;
 
 Set<Vec> walls = new HashSet<>();
 
-record ProgramState(Vec pos, int cheat) {}
+record ProgramState(Vec pos) {}
 
 Map<ProgramState, Long> visited = new HashMap<>();
 Vec end;
@@ -31,14 +31,11 @@ record GameState(ProgramState programState, long ps){
             var nextPos = programState.pos.add(dir);
             if(1 > nextPos.x || width-1 <= nextPos.x) continue;
             if(1 > nextPos.y || height-1 <= nextPos.y) continue;
-            int nextCheat = switch (programState.cheat) {
-                case 0 -> walls.contains(nextPos) ? 1 : 0;
-                case 1 -> walls.contains(nextPos) ? 3 : 2;
-                case 2 -> walls.contains(nextPos) ? 3 : 2;
-                default -> 3;
-            };
-            if(nextCheat > 2) continue;
-            var next = new GameState(new ProgramState(nextPos, nextCheat), ps+1);
+            if(walls.contains(nextPos)){
+                if(wallsHit != null) wallsHit.add(nextPos);
+                continue;
+            }
+            var next = new GameState(new ProgramState(nextPos), ps+1);
             result.add(next);
         }
         return result;
@@ -52,7 +49,7 @@ String display(GameState gameState, Set<Vec> walls){
         for (int x = 0; x < width; x++) {
             var check = new Vec(x,y);
             if(gameState.programState.pos.equals(check)){
-                sb.append(gameState.programState.cheat);
+                sb.append('0');
             }else if(walls.contains(check )){
                 sb.append('#');
             } else {
@@ -65,7 +62,7 @@ String display(GameState gameState, Set<Vec> walls){
 }
 
 void main() throws Exception {
-    var lines = Files.readAllLines(Path.of("test.txt"));
+    var lines = Files.readAllLines(Path.of("input.txt"));
 
     Vec start = null;
     for (String line : lines) {
@@ -81,16 +78,9 @@ void main() throws Exception {
     println("start: " + start);
     println("end: " + end);
     println(walls);
-    PriorityQueue<GameState> queue = new PriorityQueue<>((lhs, rhs) -> {
-        int cheatComp = Integer.compare(lhs.programState.cheat, rhs.programState.cheat);
-        if(cheatComp == 0){
-            return Long.compare(lhs.ps, rhs.ps);
-        }
-        return cheatComp;
-    }
-    );
+    PriorityQueue<GameState> queue = new PriorityQueue<>((lhs, rhs) -> Long.compare(lhs.ps, rhs.ps));
 
-    var startState = new GameState(new ProgramState(start, 0), 0);
+    var startState = new GameState(new ProgramState(start), 0);
     queue.add(startState);
     visited.put(startState.programState, 0L);
     long noCheatTime = Long.MAX_VALUE;
@@ -98,18 +88,15 @@ void main() throws Exception {
     Set<Vec> wallsHit = new HashSet<>();
     while(!queue.isEmpty()){
         var curr = queue.poll();
-        println(display(curr, walls));
+        // println(display(curr, walls));
         // if(readln(""+curr).equals("q")) break;
         if(curr.ps >= noCheatTime){
             continue;
         }
         if(curr.programState.pos.equals(end)){
-            if(curr.programState.cheat == 0){
-                println("found noCheatTime: " + curr.ps);
-                noCheatTime = curr.ps;
-                break;
-            }
-            continue;
+            println("found noCheatTime: " + curr.ps);
+            noCheatTime = curr.ps;
+            break;
         }
         for(var next : curr.nextStates(walls, width, height, wallsHit)){
             if(visited.containsKey(next.programState)/*  && visited.get(next.programState) < next.ps */){
@@ -121,29 +108,27 @@ void main() throws Exception {
     }
 
     long total = 0;
-    for (Vec wall : new HashSet<>(walls)) {
+    for (Vec wall : wallsHit) {
         walls.remove(wall);
 
-        startState = new GameState(new ProgramState(start, 0), 0);
+        startState = new GameState(new ProgramState(start), 0);
         queue.clear();
         queue.add(startState);
         visited.clear();
         visited.put(startState.programState, 0L);
         while(!queue.isEmpty()){
             var curr = queue.poll();
-            println(display(curr, walls));
-            if(readln(""+curr).equals("q")) break;
-            if(curr.ps >= noCheatTime - 0){
+            // println(display(curr, walls));
+            // if(readln(""+curr).equals("q")) break;
+            if(curr.ps >= noCheatTime - 100){
                 continue;
             }
             if(curr.programState.pos.equals(end)){
-                if(curr.programState.cheat == 0){
-                    println("found cheatTime: " + curr.ps);
-                    total++;
-                }
-                continue;
+                println("found cheatTime: " + curr.ps);
+                total++;
+                break;
             }
-            for(var next : curr.nextStates(walls, width, height, wallsHit)){
+            for(var next : curr.nextStates(walls, width, height, null)){
                 if(visited.containsKey(next.programState)/*  && visited.get(next.programState) < next.ps */){
                     continue;
                 }
